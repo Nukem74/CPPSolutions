947096520d24846575b2a5bb0573fdb71648824c
Nukem74 <Nukem74@gmail.com>
2019-04-03 21:04:43 +0500

    fifth commit

--- DP4/main.cpp
+++ DP4/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	int n, a;
-	printf("hello");
+	cin >> n;
+	cout << n;
+	cin >> a;
+	cout << a;
+	cout << n % a;
 	return 0;
 }
