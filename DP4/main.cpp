42c558b5bb66a641cd91d164f6103c04d28aa830
Nukem74 <Nukem74@gmail.com>
2019-04-03 19:47:13 +0500

    please let understand

---
+++ DP4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("Hello Git");
+	printf("Gods of Dev, please help me!");
+	return 0;
+}
