ce7c7ae458a86774d6ebd4b87dfd697f13b1ee6a
Nukem74 <Nukem74@gmail.com>
2019-04-04 08:23:06 +0500

    DP9 problem solved

--- DP9/main.cpp
+++ DP9/main.cpp
@@ -4,5 +4,22 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int foursquare, upperlefttriangle, lowerrighttrianlge;
+	foursquare = 3223;
+	upperlefttriangle = foursquare % 100;
+	foursquare = foursquare / 100;
+	lowerrighttrianlge = foursquare % 10;
+	foursquare = foursquare / 10;
+	lowerrighttrianlge = lowerrighttrianlge * 10;
+	lowerrighttrianlge = lowerrighttrianlge + (foursquare % 10);
+	if (lowerrighttrianlge == upperlefttriangle)
+	{
+		cout << "Is palindrome";
+	}
+	else
+	{
+		cout << "Is NOT palindrome";
+	}
+	
 	return 0;
 }
