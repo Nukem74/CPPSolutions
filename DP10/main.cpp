e3074f94aa3a4b4b7f396ccce03d3b83c4ce042b
Nukem74 <Nukem74@gmail.com>
2019-04-04 08:34:25 +0500

    DP10 problem solved

--- DP10/main.cpp
+++ DP10/main.cpp
@@ -3,6 +3,24 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	int luckystrike, upperstrike, lowerstrike;
+	luckystrike = 1415;
+	lowerstrike = 0;
+	upperstrike = 0;
+	lowerstrike += luckystrike % 10;
+	luckystrike = luckystrike / 10;
+	lowerstrike += luckystrike % 10;
+	luckystrike = luckystrike / 10;
+	upperstrike += luckystrike % 10;
+	luckystrike = luckystrike / 10;
+	upperstrike += luckystrike % 10;
+	if (lowerstrike == upperstrike)
+	{
+		cout << "Lucky!";
+	}
+	else
+	{
+		cout << "bad luck =(";
+	}
 	return 0;
 }
