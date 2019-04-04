9b4a5257e6d5be74fdf888e213d80547955c6b0f
Nukem74 <Nukem74@gmail.com>
2019-04-04 07:45:36 +0500

    DP6 problem solved

---
+++ DP6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	double first, second;
+	first = 10001.1;
+	second = 10001.0;
+	if (first > second)
+	{
+		cout << first;
+	}
+	else
+	{
+		cout << second;
+	}
+	
+	return 0;
+}
