2836404395eb9f20d606362182fd39bc9da15a56
Nukem74 <Nukem74@gmail.com>
2019-04-04 07:08:01 +0500

    problem solved

--- DP4/main.cpp
+++ DP4/main.cpp
@@ -3,11 +3,18 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int n, a;
-	cin >> n;
+	int n, a, r;
+	r = 0;
+	a = 10;
+	n = 314;
 	cout << n;
-	cin >> a;
-	cout << a;
-	cout << n % a;
+	r = n % 10;
+	r = r * 10;
+	n = n / 10;
+	r = r + (n % 10);
+	r = r * 10;
+	n = n / 10;
+	r = r + (n % 10);
+	cout << r;
 	return 0;
 }
