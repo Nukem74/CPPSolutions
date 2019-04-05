ca98c7fd6c615d8e65d7c133f44d779e21d67521
Nukem74 <Nukem74@gmail.com>
2019-04-05 20:13:09 +0500

    DP5 refactoring try

--- DP5/main.cpp
+++ DP5/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*ƒано натуральное число меньше 16. ѕосчитать количество его единичных битов.*/
 
 int main(int argc, char** argv) {
-	int decimal, result;
-	decimal = 11;
+	int decimal;
+	cout << "Enter a number lesser than 16:_";
+	cin >> decimal;
 	cout << decimal;
-	result = 0;
-	result += decimal % 2;
-	decimal = decimal / 2;
-	result += decimal % 2;
-	decimal = decimal / 2;
-	result += decimal % 2;
-	decimal = decimal / 2;
-	result += decimal % 2;
-	decimal = decimal / 2;
+	cout << "\n";
+	int result = 0;
+	for(int i = 0; i < 4;i++)
+	{
+		result += decimal % 2;
+		decimal = decimal / 2;
+	}
+	cout << "There are ";
 	cout << result;
+	cout << " ones in binary representation of your number";
 
 	
 	return 0;
