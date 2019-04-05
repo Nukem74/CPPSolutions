de38c48d443ab0475fede9af29f66310b375cc03
Nukem74 <Nukem74@gmail.com>
2019-04-05 07:38:56 +0500

    DP15 start & problem solved

---
+++ DP16/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int number, counter;
+	counter = 0;
+	cout << "Set a number";
+	cin >> number;
+	for(int i = 1; i != number; i++)
+	{
+		if(number % i == 0)
+		{
+			counter++;
+		}
+	}
+	cout << counter;
+	return 0;
+}
