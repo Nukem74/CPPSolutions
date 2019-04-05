40ac74a3a0a80574966954c81c6ba096338603cf
Nukem74 <Nukem74@gmail.com>
2019-04-05 19:41:06 +0500

    DP4 refactoring

--- DP4/main.cpp
+++ DP4/main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+/*Сформировать число, представляющее собой реверсную (обратную в порядке следования разрядов) 
+запись заданного трехзначного числа. */
 int main(int argc, char** argv) {
-	int n, a, r;
-	r = 0;
-	a = 10;
-	n = 314;
-	cout << n;
-	r = n % 10;
-	r = r * 10;
-	n = n / 10;
-	r = r + (n % 10);
-	r = r * 10;
-	n = n / 10;
-	r = r + (n % 10);
-	cout << r;
+	int number; //Инициализация обрабатываемой переменной
+	cout << "Enter 3 digits to reverse: ";
+	cin >> number;
+	cout<< "\n";
+	int result = 0; //Инициализация переменной результата
+	int aloe = 10;// Инициализация делителя
+	for(int i = 0; i < 3; i++)
+	{
+		//Тело цикла разворачивающего 3х значное число
+		result *= aloe;
+		result += number % aloe;
+		number /= aloe;
+	}
+	cout << "Reversed: ";
+	cout << result;
 	return 0;
 }
