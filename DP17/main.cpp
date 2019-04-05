405e4e705ed319912710e9db4e739771eebcd1d7
Nukem74 <Nukem74@gmail.com>
2019-04-05 07:46:42 +0500

    DP17 problem solved

--- DP17/main.cpp
+++ DP17/main.cpp
@@ -3,6 +3,24 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	int number, counter;
+	counter=0;
+	cout << "Give me a number";
+	cin >> number;
+	for(int i = 2; i != number; i++)
+	{
+		if(number % i == 0)
+		{
+			counter++;
+		}
+	}
+	if(counter == 0)
+	{
+		cout << "Is simple number";
+	}
+	else
+	{
+		cout << "Is not simple number";
+	}
 	return 0;
 }
