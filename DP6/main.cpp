f6c7e21b4d80784a9c1aaa49186619bfa0bbc53f
Nukem74 <Nukem74@gmail.com>
2019-04-05 20:20:31 +0500

    DP6 refactoring

--- DP6/main.cpp
+++ DP6/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+/*ƒаны два числа. ¬ывести на экран то из них, которое больше.*/
 int main(int argc, char** argv) {
 	double first, second;
-	first = 10001.1;
-	second = 10001.0;
+	cout << "Enter first number:";
+	cin >> first;//инициализаци€ первого числа
+	cout <<"Enter second number";
+	cin >> second;//инициализаци€ второго число
 	if (first > second)
 	{
 		cout << first;
