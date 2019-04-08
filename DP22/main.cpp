2f53382442901b25528c2b86d43d8c71ff6298a7
Nukem74 <Nukem74@gmail.com>
2019-04-08 18:33:19 +0500

    DP22 Start

---
+++ DP22/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*Даны два натуральных числа. Найти их наибольший общий делитель.*/
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
