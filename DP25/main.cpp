17f42664a8671b3078876d6797766d56a0d3dcb6
Nukem74 <Nukem74@gmail.com>
2019-04-08 19:01:40 +0500

    DP25 start

---
+++ DP25/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/*Даны натуральные числа x и n. Вычислить xn, используя алгоритм быстрого возведения в степень:*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
