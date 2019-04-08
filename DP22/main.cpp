74682177e7a46b35a7b03d8d7e521962eb6d6ec0
Nukem74 <Nukem74@gmail.com>
2019-04-08 18:37:47 +0500

    DP22 problem solved

--- DP22/main.cpp
+++ DP22/main.cpp
@@ -5,6 +5,22 @@ using namespace std;
 /*Даны два натуральных числа. Найти их наибольший общий делитель.*/
 
 int main(int argc, char** argv) {
-	
+	cout << "Enter two numbers to find their largest common divider";
+	int first, second;//Initialising operators
+	cout << "\n";
+	cout << "Enter first number ";
+	cin >> first;
+	cout << "\n";
+	cout << "Enter second number ";
+	cin >> second;
+	int temp;
+	for(int i = 1;i != first;i++)
+	{
+		if(first % i == 0 && second % i == 0)
+		temp = i;
+	}
+	cout << "\n";
+	cout << "The largest common divider is ";
+	cout << temp;
 	return 0;
 }
