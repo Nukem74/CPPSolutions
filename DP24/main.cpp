6dc48018b2a0acc3c53e94ab93411410a03600b7
Nukem74 <Nukem74@gmail.com>
2019-04-08 18:59:40 +0500

    DP24 problem solved

--- DP24/main.cpp
+++ DP24/main.cpp
@@ -6,7 +6,21 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-
-	
+	cout << "Enter base and index to get power";
+	int base, index, power;
+	power = 1;
+	cout << "\n";
+	cout << "Enter a base ";
+	cin >> base;
+	cout << "\n";
+	cout << "Enter an index ";
+	cin >> index;
+		for(int i = 0; i < index; i++)
+	{
+		power = power * base;
+	}
+	cout << "\n";
+	cout << "THE K'OS IS PAWAH ";
+	cout >> power;
 	return 0;
 }
