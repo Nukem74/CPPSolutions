cd3a4a84ec58f50bed6bdac037b4c2754af93fb9
Nukem74 <Nukem74@gmail.com>
2019-04-09 06:55:06 +0500

    DP28 start

---
+++ DP28/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*Дано натуральное число n (которое также может быть равно нулю). Вычислить n!*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	
+	return 0;
+}
