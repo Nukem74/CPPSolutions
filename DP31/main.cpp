32118932ccefa046dc7f8b153f66a74508e37571
Nukem74 <Nukem74@gmail.com>
2019-04-09 07:28:22 +0500

    DP31 start

---
+++ DP31/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*Формулировка. Дано натуральное число n заранее неизвестной разрядности. 
+  Сформировать и вывести на экран число, представляющее собой реверсную запись n.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
