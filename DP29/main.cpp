22dbcee112cb055fd449ba198693b28d1be698a0
Nukem74 <Nukem74@gmail.com>
2019-04-09 07:18:45 +0500

    DP29 problem solved

--- DP29/main.cpp
+++ DP29/main.cpp
@@ -5,5 +5,26 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	cout << "Enter two numbers, first number must be larger then second";
+	int first, second;//Initialising operators
+	cout << "\n";
+	cout << "Enter first number ";
+	cin >> first;
+	cout << "\n";
+	cout << "Enter second number ";
+	cin >> second;
+	int result, numenator, denominator;//Initialising parametrs
+	numenator = first;
+	denominator = 1;
+	for(int i = 1;i != second + 1; i++)
+	{
+		numenator *= (first - i);//Solving
+		denominator *= i;
+	}
+	result = numenator / denominator;//Calculating result
+	cout << "\n";
+	cout << "The result is ";
+	cout << result;
+	
 	return 0;
 }
