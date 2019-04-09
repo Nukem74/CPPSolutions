856866436ab3aafba163473e66e5149aa1863b1e
Nukem74 <Nukem74@gmail.com>
2019-04-09 06:48:52 +0500

    DP27 problem solved

--- DP27/main.cpp
+++ DP27/main.cpp
@@ -6,7 +6,25 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "To solve multiargumental equation like (an*x^n + a(n-1)*x^(n-1) + ... + a0 = 0) \n input index of multiarumental: /n n=";
-	int result, par;
+	cout << "To solve multiargumental equation like (an*x^n + a(n-1)*x^(n-1) + ... + a0) \n input index of multiargumental: \n n=";
+	double result, par, arg;//initialising parametrs
+	int index;//initialising index
+	cin >> index;
+	result = 0;
+	cout << "\n";
+	cout << "Enter an argument x = ";
+	cin >> arg;//initialising argument
+	for(int i = 0;i != index; i++)
+	{
+		cout << "Enter ";
+		cout << i;
+		cout << " member of multiargumental ";
+		cin >> par;//initialising parametr
+		result = result + (par * arg);
+	}
+	
+	cout << "result is ";
+	cout << result;
+	
 	return 0;
 }
