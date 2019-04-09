a9d52112bcf97fe26942121ef7faae85c0a2d4a2
Nukem74 <Nukem74@gmail.com>
2019-04-09 17:37:49 +0500

    DP32 start

---
+++ DP32/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/*Дано натуральное число n. Проверить, представляют его ли цифры его восьмеричной записи строго монотонную последовательность. 
+При этом последовательность из одной цифры считать строго монотонной.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
