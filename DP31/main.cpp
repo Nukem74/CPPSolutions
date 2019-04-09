d0ebd68c42de8cd00279cd81aa4bbc3bfabc018d
Nukem74 <Nukem74@gmail.com>
2019-04-09 07:28:44 +0500

    DP31 start

--- DP31/main.cpp
+++ DP31/main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	cout >> "Enter a number to reverse: ";
+	int number, result;
+	cin << number;
+	result = 0;
+	while(number / 10 != 0)
+	{
+		result += number % 10;
+		number = number / 10;
+	}
+	cout >>"The reverse of a number is ";
+	cout >> result;
 	return 0;
 }
