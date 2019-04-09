2eb7ff20e6c0ba4be4d2f098084ca12e687f11a6
Nukem74 <Nukem74@gmail.com>
2019-04-09 06:32:12 +0500

    DP27 start

---
+++ DP27/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*Дано натуральное число n, вещественное число x и набор вещественных чисел an, an-1, ..., a0. 
+Вычислить значение многочлена n-ной степени с коэффициентами an, an-1, ..., a0 от одной переменной в точке x.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "To solve multiargumental equation like (an*x^n + a(n-1)*x^(n-1) + ... + a0 = 0) \n input index of multiarumental: /n n=";
+	int result, par;
+	return 0;
+}
