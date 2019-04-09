fa543eaf080d81a62b58827b70be3a94a847a8f8
Nukem74 <Nukem74@gmail.com>
2019-04-09 06:06:04 +0500

    DP26 start

---
+++ DP26/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+/*Дано натуральное число n. Вывести на экран решения всех квадратных уравнений вида x^2 + 2ax - 3 = 0 для всех a от 1 до n.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
