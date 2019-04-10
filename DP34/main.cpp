823c24373448df0fe115949b2f3a12594ce24b9b
Nukem74 <Nukem74@gmail.com>
2019-04-10 07:45:48 +0500

    DP34 problem solved

--- DP34/main.cpp
+++ DP34/main.cpp
@@ -6,6 +6,24 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	cout << "Enter 2 numbers with same length";
+	int first, second, result;//initialising operators
+	cout << "\nEnter first number ";
+	cin >> first;
+	cout << "\nEnter second number ";
+	cin >> second;
+	result = 0;//initialising storage variable
+	while(first != 0)
+	{
+		result *= 10;
+		result += first % 10;
+		first = first / 10;
+		result *= 10;
+		result += second % 10;
+		second = second / 10;
+	}
+	cout << "\n Result is ";
+	cout << result;
 	
 	return 0;
 }
