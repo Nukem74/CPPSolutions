185711650545003d21618de74f3bc7f4d1e0a767
Nukem74 <Nukem74@gmail.com>
2019-04-10 07:48:20 +0500

    DP35 start

---
+++ DP35/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/*Даны натуральные числа x и n (n <= 10). Вывести на экран число x, записанное в системе счисления с основанием n.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
