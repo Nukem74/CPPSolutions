940895b694a1738d916f63d0168b4f619cc5aa86
Nukem74 <Nukem74@gmail.com>
2019-04-10 07:35:32 +0500

    DP34 start

---
+++ DP34/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*Даны два натуральных числа одинаковой десятичной разрядности. Сформировать из них третье число так, 
+чтобы цифры первого числа стояли на нечетных местах третьего, а цифры второго - на четных. При этом порядки следования цифр сохраняются. 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
