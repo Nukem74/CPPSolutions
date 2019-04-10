282c8fccb9e66ae845b7718be867cc335c84f39b
Nukem74 <Nukem74@gmail.com>
2019-04-10 08:01:43 +0500

    DP35 problem solved

--- DP35/main.cpp
+++ DP35/main.cpp
@@ -5,5 +5,28 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	cout << "Enter a number, and a base";
+	int number, base;//initialising operators
+	cout << "\nEnter a number ";
+	cin >> number;
+	cout << "\nEnter a base ";
+	cin >> base;
+	int  result, temp;//initialising parametrs
+	result = 0;
+	while(number != 0)//reducing number to base
+	{
+		result *= 10;
+		result += number % base;
+		number = number / base;
+	}
+	temp = result;
+	result = 0;
+	while(temp != 0)//reverting result
+	{	
+		result *= 10;
+		result += temp % 10;
+		temp = temp / 10;
+	}
+	cout << result;
 	return 0;
 }
