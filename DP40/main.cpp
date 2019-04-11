184b286c88bc1fa76de3b3e1b98e122c2d85b70b
Nukem74 <Nukem74@gmail.com>
2019-04-11 15:57:30 +0500

    DP40 problem solved

--- DP40/main.cpp
+++ DP40/main.cpp
@@ -6,6 +6,25 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << ""
+	cout << "Enter a number ";
+	int number, result;//initialising variable
+	cin >> number;
+	result = 1;
+	for(int i = 1; number != 0; i++)
+	{
+		if(number % 10 == 0)
+		{
+			break;
+		}
+		if(i % 2 == 0)
+		{
+			result = result * (number % 10);
+			cout << result;
+		}
+		number = number / 10;
+	}
+	cout << "\nResult is ";
+	cout << result;
+	
 	return 0;
 }
