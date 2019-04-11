f3dbf5840070790e6a5c3f781cb98a7cdad56dd5
Nukem74 <Nukem74@gmail.com>
2019-04-11 19:03:41 +0500

    DP41 problem solved

---
+++ DP41/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*ƒано натуральное число n, а затем последовательность натуральных чисел, ограниченна€ вводом нул€. 
+¬ывести на экран произведение двузначных элементов этой последовательности, которые дел€тс€ на n.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "Enter "
+	return 0;
+}
