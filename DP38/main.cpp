6350294d43cc360a022a3d58e23615cb51419583
Nukem74 <Nukem74@gmail.com>
2019-04-11 15:04:44 +0500

    DP38 start

---
+++ DP38/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/*ƒано натуральное число n. ѕроверить, представл€ет ли собой палиндром его дес€тична€ запись.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
