8ab99f5368f0ccf063695e3b6ede3ce653e3e57c
Nukem74 <Nukem74@gmail.com>
2019-04-11 15:32:48 +0500

    DP39 problem solved

--- DP39/main.cpp
+++ DP39/main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	cout << "Enter a number ";
+	int number;//initialising variable
+	cin >> number;
+	while(number != 1)
+	{
+		if(number % 2 != 0)//checking dividency
+		{
+			cout << "\nIs not 2-based";
+			number = 0;
+			break;
+		}
+		number = number / 2;
+	}
+	if(number != 0)//displaying positive answer
+		cout << "Is 2-based";
 	
 	return 0;
 }
