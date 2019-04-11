00aadb0d94f44d0a4daeca8fbd792a7bf118d062
Nukem74 <Nukem74@gmail.com>
2019-04-11 15:42:39 +0500

    DP40 start

---
+++ DP40/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*Дана последовательность натуральных чисел, ограниченная вводом нуля. Вывести на экран произведение четных элементов этой последовательности. 
+При этом ноль не считается членом последовательности.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << ""
+	return 0;
+}
