f09272b6ecf6ea32b9023c99c11d7baadd6519a8
Nukem74 <Nukem74@gmail.com>
2019-04-11 15:25:23 +0500

    DP39 start

---
+++ DP39/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/*ƒано натуральное число n. ѕроверить, представл€ет ли оно собой натуральную степень числа 2.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
