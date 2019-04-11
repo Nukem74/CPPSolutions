26943ccb5775946cab5ae0ae2b8c754f802711de
Nukem74 <Nukem74@gmail.com>
2019-04-11 19:07:27 +0500

    DP43 start

---
+++ DP43/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*Дана последовательность натуральных чисел, ограниченная вводом нуля. 
+Проверить, начинается ли каждый из ее членов (со второго) с десятичной цифры, на которую оканчивается предыдущий.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "Enter a sequnece of numbers ";
+	
+	return 0;
+}
