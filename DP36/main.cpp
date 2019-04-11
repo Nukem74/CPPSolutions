f2a7b5bc2b9bd5fc5e24a1b38dccac38e6e1a33e
Nukem74 <Nukem74@gmail.com>
2019-04-11 14:07:21 +0500

    DP36 start

---
+++ DP36/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*Даны натуральные числа m и n. Вывести на экран их наименьший нетривиальный делитель или сообщить, что его нет. */
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	
+	return 0;
+}
