3e78efa160baf5f4a900b364f08cb5ec8336f0a0
Nukem74 <Nukem74@gmail.com>
2019-04-11 14:23:07 +0500

    DP36 problem solved

--- DP36/main.cpp
+++ DP36/main.cpp
@@ -5,7 +5,38 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
-	
+	cout << "Enter two numbers to find their lesser untrivial divider ";
+	int first, second;
+	cout << "\nEnter first number ";
+	cin >> first;
+	cout << "\nEnter second number ";
+	cin >> second;
+	int lesnum = 0;
+	if(first > second)
+	{
+		lesnum = second;
+	}
+	if(first < second)
+	{
+		lesnum = first;
+	}
+	bool chkbox = false;
+	for(int i = 2; i <= lesnum; i++)
+	{
+		if(first % i == 0)
+		{
+			if(second % i == 0)
+			{
+				cout << "\nLesser untrivial divider is ";
+				cout << i;
+				chkbox = true;
+				break;
+			}
+		}
+	}
+	if(!chkbox)
+	{
+		cout << "\nThere is no common untrivial divider";	
+	}	
 	return 0;
 }
