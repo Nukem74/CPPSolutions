80b446b14dd9692755e837e69b7b8bf77ee16349
Nukem74 <Nukem74@gmail.com>
2019-04-12 07:24:38 +0500

    true DP45 problem solved

--- DP45/main.cpp
+++ DP45/main.cpp
@@ -5,5 +5,31 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	cout << "Enter a sequence of numbers ";
+	int number, alfa, beta, delta;
+	cout << "\nEnter first number ";
+	cin >> number;
+	alfa = number;
+	cout << "\nEnter new number ";
+	cin >> number;
+	beta = number;
+	delta = beta - alfa;
+	while(number != 0)
+	{
+		alfa = beta;
+		cout << "\nEnter new number ";
+		cin >> number;
+		beta = number;
+		if((delta * (beta - alfa) > 0))
+		{
+			cout << "sequence is dull";
+		}
+		else
+		{
+			cout << "sequence is NOT dull";
+		}
+		delta = beta - alfa;
+	}
+
 	return 0;
 }
