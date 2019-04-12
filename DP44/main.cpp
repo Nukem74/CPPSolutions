ee08b2364e9bfc4cc5543849e47cf6b3682b48d1
Nukem74 <Nukem74@gmail.com>
2019-04-12 06:46:52 +0500

    DP45 problem solved

--- DP44/main.cpp
+++ DP44/main.cpp
@@ -5,6 +5,29 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	cout << "Enter a sequence of numbers ";
+	int number, first, second, third, temp;
+	cout << "\nEnter first number ";
+	cin >> number;
+	first = number;
+	cout << "\nEnter new number ";
+	cin >> number;
+	second = number;
+	while(number != 0)
+	{
+		cout << "\nEnter a new number ";
+		cin >> number;
+		third = number;
+		if(((second - first)*(second - third)) > 0)
+		{
+			cout << "\nTexas chaisaw slaughter";
+		}
+		else
+		{
+			cout << "\nBeethoven";
+		}
+		first = second;
+		second = third;
+	}
 	return 0;
 }
