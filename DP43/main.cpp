ce9478d09d0453c7f2c346893f6b24d83591b610
Nukem74 <Nukem74@gmail.com>
2019-04-12 06:03:15 +0500

    DP43 start

--- DP43/main.cpp
+++ DP43/main.cpp
@@ -7,6 +7,29 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	cout << "Enter a sequnece of numbers ";
-	
+	int number = 1;
+	int first, last;
+	last = first;
+	while(number != 0)
+	{
+		cout << "\nEnter new number ";
+		cin >> number;
+		first = number % 10;
+		if(first == last)
+		{
+			cout << "\nIt start where that ends";
+		}
+		else
+		{
+			cout << "\nIt starts from nowhere";
+		}
+		while(number != 0)
+		{
+			last = number % 10;
+			number = number / 10;
+		}
+		number = 1;
+		
+	}
 	return 0;
 }
