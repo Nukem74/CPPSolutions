229a5144018b864792a21bc666e00fcbde293b9e
Nukem74 <Nukem74@gmail.com>
2019-04-12 14:08:19 +0500

    DP49 problem solved

--- DP49/main.cpp
+++ DP49/main.cpp
@@ -5,5 +5,23 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	char letter;
+	int counter;
+	while(letter != '0')
+	{
+		cin >> letter;
+		if(letter == '(')
+		{
+			counter++;
+		}
+		if(letter == ')')
+		{
+			counter--;
+		}	
+	}
+	if(counter != 0)
+	{
+		cout << "\nUnbalanced parenthesis";
+	}
 	return 0;
 }
