c8c02c9cc1af6bde857892604ca928530a03562e
Nukem74 <Nukem74@gmail.com>
2019-04-12 06:50:29 +0500

    DP45 start

---
+++ DP45/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/*ƒана последовательность натуральных чисел, ограниченна€ вводом нул€. ѕроверить, €вл€етс€ ли эта последовательность строго монотонной.*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
