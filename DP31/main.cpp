049cfafa1f983e3991e4693821b1cbcac3ceeaa0
Nukem74 <Nukem74@gmail.com>
2019-04-15 08:44:15 +0500

    objective attempt 1 at DP 31

--- DP31/main.cpp
+++ DP31/main.cpp
@@ -5,11 +5,24 @@ using namespace std;
   —формировать и вывести на экран число, представл€ющее собой реверсную запись n.*/
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int reverted(int n)
+{
+	int number = n;
+	int rebmun = 0;
+	while(number != 0)
+	{
+		rebmun = rebmun * 10;
+		rebmun = rebmun + number % 10;
+		number = number / 10;
+	}
+	return rebmun;
+}
+
 int main(int argc, char** argv) {
 	cout << "Enter a number to reverse: ";
 	int number, result;//initialising parametrs
 	cin >> number;
-	result = 0;
+	/*result = 0;
 	while(number / 10 != 0)
 	{
 		result *= 10;
@@ -19,6 +32,7 @@ int main(int argc, char** argv) {
 	result *= 10;
 	result += number % 10;
 	cout <<"\nThe reverse of a number is ";
-	cout << result;
+	cout << result;*/
+	cout << reverted(number);
 	return 0;
 }
