93e49c9a8e15bd840d383c29ff666cb4e69e31fb
Nukem74 <Nukem74@gmail.com>
2019-04-15 11:54:14 +0500

    matrix determinant 1

--- MatrixDeterminant/main.cpp
+++ MatrixDeterminant/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+double initsquarematrix[](int n)
+{
+	int size = n;
+	double matrix [size][size];
+	return matrix;
+}
+
+
 int main(int argc, char** argv) {
+	initsquarematrix(4);
 	return 0;
 }
