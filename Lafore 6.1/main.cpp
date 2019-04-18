49d6a0100eb1730ea308e63f3db340b94c928eed
Nukem74 <Nukem74@gmail.com>
2019-04-18 17:31:58 +0500

    Lafore 6.1 problem solved

--- Lafore 6.1/main.cpp
+++ Lafore 6.1/main.cpp
@@ -2,31 +2,35 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-class Int
+class INT								//определение класса
 {
-	private:
-		int _intgr;
-	public:
-		void setzero()
+	private:							//перечисление приватных членов
+		int _intgr;						//целочисленное приватное поле
+	public:								//перечисление публичных членов
+		void setzero()					//метод задающий значение приватного поля
 		{
 			_intgr = 0;
 		}
-		void setValue(int v)
+		void setValue(int v)			//метод задающий значение приватного поля
 		{
 			_intgr = v;
 		}
-		void dispValue()
+		void dispValue()				//метод отображающий в окне консоли значение приватного поля
 		{
 			cout << _intgr;
 		}
-		int sumValue(int a, int b)
+		int sumValue(INT a, INT b)		//метод возвращающий целочисленное значение и принимающий в качестве аргументов два объекта класса INT
 		{
-			return(a+b);
+			_intgr = a._intgr+b._intgr;
 		}
 };
 
 int main(int argc, char** argv) {
-	Int A, B, C;
-	
+	//Демонстратор работы класса
+	INT A, B, C;
+	A.setValue(3);
+	B.setValue(7);
+	C.sumValue(A,B);
+	C.dispValue();
 	return 0;
 }
