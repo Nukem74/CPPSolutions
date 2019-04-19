3368dcfbc42a26add06492855975496c0fc41c67
Nukem74 <Nukem74@gmail.com>
2019-04-19 18:11:51 +0500

    Lafore 6.9 start

---
+++ Lafore 6.9/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+class fraction
+{
+	private:						//перечисление приватных членов
+		int _numerator;
+		int _denominator;
+	public:							//перечисление публичных членов
+		fraction(int n, int d): _numerator(n), _denominator
+		{
+			//empty
+		}
+		
+};
+
+int main(int argc, char** argv) {
+	return 0;
+}
