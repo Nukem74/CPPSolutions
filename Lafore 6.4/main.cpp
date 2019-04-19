a8ff8589d836caa085330bf3264f56e93e101b99
Nukem74 <Nukem74@gmail.com>
2019-04-19 08:11:46 +0500

    Lafore 6.4 class described

--- Lafore 6.4/main.cpp
+++ Lafore 6.4/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+class employee
+{
+	private:						//перечисление приватных членов	
+		int _ID;
+		float _salary;
+	public:							//перечисление публичных членов
+		employee()					//конструктор без аргументов
+		{
+			//empty
+		}
+		void setID(int i)
+		{
+			_ID = i;
+		}
+		void setSAL(int s)
+		{
+			_salary = s;
+		}
+		void dispEmp()
+		{
+			cout << _ID << ':' << _salary << '$' << endl;
+		}
+};
 int main(int argc, char** argv) {
 	return 0;
 }
